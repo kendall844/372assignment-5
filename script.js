c58f424e06455f56b556f142b17fc518e3779039
Kendall Williford <KMWILLIFORD@uncg.edu>
2026-03-22 11:55:59 -0400

    Added async func fetchRepos and clear function

--- script.js
+++ script.js
@@ -2,32 +2,51 @@ const gallery = document.getElementById("gallery");
 const searchBtn = document.getElementById("searchBtn");
 const enterUser = document.getElementById("username");
 
-window.addEventListener("DOMContentLoaded", ()=> {
+window.addEventListener("DOMContentLoaded", () => {
     fetchRepos("kendall844");
 });
 
 searchBtn.addEventListener("click", () => {
     const username = enterUser.ariaValueMax.trim();
-    if(username){
+    if (username) {
         fetchRepos(username);
     }
 });
 
-async function fetchRepos(username){
+function clearGallery() {
+    while (gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+}
+
+async function fetchRepos(username) {
     clearGallery();
     gallery.textContent = "Loading...";
 
-    try{
+    try {
         const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=10`);
         const repos = await response.json();
 
         clearGallery();
 
-        if(!repos || repos.length === 0){
+        if (!repos || repos.length === 0) {
             gallery.textContent = "No repositories found.";
             return;
         }
+
+        for (let i = 0; i < repos.length; i++) {
+            let repo = repos[i];
+
+            const languages = await fetchLang(repo.languages_url);
+            const repoCard = createRepoCard(repo, languages);
+            gallery.appendChild(repoCard);
+        }
+    } catch (error) {
+        clearGallery();
+        gallery.textContent = "Error: Could not fetch data.";
+        console.error(error);
     }
 }
+
 
 
