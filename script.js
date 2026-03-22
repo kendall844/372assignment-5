1923e320e49bf17012a54214da1baa8c14125669
Kendall Williford <KMWILLIFORD@uncg.edu>
2026-03-22 11:46:11 -0400

    Setting up JS page, before adding bulk of JS code

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const gallery = document.getElementById("gallery");
+const searchBtn = document.getElementById("searchBtn");
+const enterUser = document.getElementById("username");
 
+window.addEventListener("DOMContentLoaded", ()=> {
+    fetchRepos("kendall844");
+});
+
+searchBtn.addEventListener("click", () => {
+    const username = enterUser.ariaValueMax.trim();
+    if(username){
+        fetchRepos(username);
+    }
+});
+
+async function fetchRepos(username){
+    clearGallery();
+    gallery.textContent = "Loading...";
+
+    try{
+        const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=10`);
+        const repos = await response.json();
+
+        clearGallery();
+
+        if(!repos || repos.length === 0){
+            gallery.textContent = "No repositories found.";
+            return;
+        }
+    }
+}
 
 
